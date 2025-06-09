0076a556f7b6320c2c59945acbd571bb63839ca9
archswitcher <zapeta.pablo@gmail.com>
2025-06-09 15:28:05 -0600

    first version generic modal

---
+++ agents_app/lib/widgets/commons/generic_modal.dart
@@ -0,0 +1,44 @@
+import 'package:flutter/material.dart';
+
+class GenericModal extends StatelessWidget {
+  final Widget content;
+  final VoidCallback? onAccept;
+  final VoidCallback? onCancel;
+  final String title;
+  final String acceptText;
+  final String cancelText;
+
+  const GenericModal({
+    super.key,
+    required this.content,
+    this.onAccept,
+    this.onCancel,
+    this.title = 'Confirmación',
+    this.acceptText = 'Aceptar',
+    this.cancelText = 'Cancelar',
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return AlertDialog(
+      title: Text(title),
+      content: content,
+      actions: [
+        TextButton(
+          onPressed: () {
+            onCancel?.call();
+            Navigator.of(context).pop(); // cerrar modal
+          },
+          child: Text(cancelText),
+        ),
+        ElevatedButton(
+          onPressed: () {
+            onAccept?.call();
+            Navigator.of(context).pop(); // cerrar modal
+          },
+          child: Text(acceptText),
+        ),
+      ],
+    );
+  }
+}
